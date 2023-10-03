7f4264e067503ddd07b2ed9dec7d29f110e4860f
LucasAHorn <146038502+LucasAHorn@users.noreply.github.com>
2023-10-03 12:21:38 -0500

    Added resume and better design overall
    new button
    resume
    new colors

--- README.md
+++ README.md
@@ -2,12 +2,27 @@ This is a personal website project
 
 I plan to use this website to:
  - relearn some HTML, CSS, and JavaScript
- - host games that I develop 
+ - host games that I develop (can I host games in c or java on this page?)
     - tic tac toe
     - connect 4
     - chess maybe
     - checkers
+ - create a div to hold games
+ - replace the games with a screen when clicked (have a return to games button)
  - possibly also convert spotify playlists to youtube music playlists and vice versa
  - host my resume
 
-Currently trying to impliment a time function to use elsewhere
+plan on making the website to be top to bottom:
+ - Header 
+     - Name
+     - year(freshman) | major(software Eng)
+ - resume
+ - games (still need to develop)
+ 
+CURRENT ISSUES WITH WEBSITE:
+ - none that I am aware of
+
+Features to add
+ - games (in java, c, or js)
+ - nav bar
+ - better color scheme and ideas
--- indexScript.js
+++ indexScript.js
@@ -1,14 +1,47 @@
-// Using Date objects in js
 const startOfProject = 1695775081033; //for the nostalgia
 console.log(Date.now() - startOfProject + " seconds ago I created this project");
-let x = 0;
+var x = 0;
 const a = document.getElementById("secs");
+//const timerResetButton = document.getElementById("resetTimerButton");
+let temp;
 
+//runtime Fn's
+TimePassed();
+
+//changes the str showing how much time has passed
 function TimePassed(){
     setInterval(function(){
+        x++;
         a.innerHTML = "You have been on this website for " + x + " seconds";
-        x+=1;
-        }, 1000);
+    }, 1000);
     return;
 }
-TimePassed();
+
+//resets timer (it will show zero before 1)
+function resetTimer(){
+    x = -1;
+    return;
+}
+
+//mouseOver button (ID should be th Fn input)
+function mouseOn(temp){
+    idFinder(temp);
+    temp.style.backgroundColor = "#222288";
+    temp.style.color = "#EEEEEE";
+    // temp.style.border-style = "inset";
+    return;
+}
+
+//mouseOff button (ID should be th Fn input)
+function mouseOff(temp){
+    idFinder(temp);
+    temp.style.backgroundColor = "#3333AA";
+    temp.style.color = "#EEEEEE";
+    return;
+}
+
+//used in mouseOn() and mouseOff() to get the item in doc
+function idFinder(temp){
+    temp = document.getElementById(temp);
+    return;
+}
